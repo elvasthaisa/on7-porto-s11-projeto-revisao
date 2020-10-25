dc36cc4d389121089837dfd28eb8c4e3aed6c485
elvasthaisa <thaisaelvas@gmail.com>
2020-10-25 00:31:49 -0300

    Adição de delete e patch ao servidor

--- series/src/routes/seriesRoute.js
+++ series/src/routes/seriesRoute.js
@@ -7,4 +7,7 @@ router.get('/:id', controller.getSerieById);
 router.post('/', controller.postSerie);
 router.post('/:id/season', controller.createSeason);
 router.put('/:id', controller.putSerie);
+router.delete('/:id', controller.deleteSerie);
+router.patch('/:id/liked', controller.likedSerie);
+
 module.exports = router;
